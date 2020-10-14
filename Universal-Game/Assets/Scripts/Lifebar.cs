1920250ec18f9ddd6e4814ddc98a1baaeed9e432
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-10-14 23:36:42 +0300

    Bubble and icons

--- Universal-Game/Assets/Scripts/Lifebar.cs
+++ Universal-Game/Assets/Scripts/Lifebar.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+//[ExecuteInEditMode]
 public class Lifebar : MonoBehaviour
 {
     public TextMeshProUGUI precentageTxt;
     public Image fillImg;
+    public Gradient color;
+
+    //[Range(0,1)]
+    //public float value;
 
     public void SetValue(float val)
     {
         fillImg.fillAmount = val;
         int valInt = (int)(val * 100f);
         precentageTxt.text = valInt.ToString() + "%";
+        fillImg.color = color.Evaluate(val);
     }
+
+    //private void Update()
+    //{
+    //    SetValue(value);
+    //}
 }
