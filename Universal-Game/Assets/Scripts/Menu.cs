bccd35f263dc2cdb1b04d7cc675483ed797b5db2
Richter17 <36236219+Richter17@users.noreply.github.com>
2020-10-16 00:34:19 +0300

    Build and images

--- Universal-Game/Assets/Scripts/Menu.cs
+++ Universal-Game/Assets/Scripts/Menu.cs
@@ -10,4 +10,10 @@ public class Menu : MonoBehaviour
     {
         ScenesManager.LoadScene(GameSceneID);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
 }
